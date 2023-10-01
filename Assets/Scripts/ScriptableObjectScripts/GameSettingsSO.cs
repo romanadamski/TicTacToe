4e4bf74df4cc1027f615f5edb8466793a2449dc3
romanadamski <roman.adamski@gmail.com>
2023-10-01 19:19:56 +0200

    ranged possible board size

--- Assets/Scripts/ScriptableObjectScripts/GameSettingsSO.cs
+++ Assets/Scripts/ScriptableObjectScripts/GameSettingsSO.cs
@@ -6,13 +6,19 @@ public class GameSettingsSO : ScriptableObject
     [SerializeField]
     private float playerTurnTime = 5.0f;
     public float PlayerTurnTime => playerTurnTime;
+
+	[Range(3, 10)]
 	[SerializeField]
-	private uint horizontalNodes;
+	private uint horizontalNodes = 3;
     public uint HorizontalNodes => horizontalNodes;
+
+	[Range(3, 10)]
 	[SerializeField]
-	private uint verticalNodes;
+	private uint verticalNodes = 3;
     public uint VerticalNodes => verticalNodes;
+
+	[Range(3, 10)]
 	[SerializeField]
-	private uint winNodes;
+	private uint winNodes = 3;
     public uint WinNodes => winNodes;
 }
