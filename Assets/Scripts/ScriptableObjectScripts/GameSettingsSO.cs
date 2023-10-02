975481436fa39c3ec7ba300be11696f69595d277
romanadamski <roman.adamski@gmail.com>
2023-10-02 15:34:40 +0200

    Added custom editor for game settings SO

--- Assets/Scripts/ScriptableObjectScripts/GameSettingsSO.cs
+++ Assets/Scripts/ScriptableObjectScripts/GameSettingsSO.cs
@@ -7,17 +7,14 @@ public class GameSettingsSO : ScriptableObject
     private float playerTurnTime = 5.0f;
     public float PlayerTurnTime => playerTurnTime;
 
-	[Range(3, 10)]
 	[SerializeField]
 	private uint horizontalNodes = 3;
     public uint HorizontalNodes => horizontalNodes;
 
-	[Range(3, 10)]
 	[SerializeField]
 	private uint verticalNodes = 3;
     public uint VerticalNodes => verticalNodes;
 
-	[Range(3, 10)]
 	[SerializeField]
 	private uint winNodes = 3;
     public uint WinNodes => winNodes;
